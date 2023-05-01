45aca32cb34bf1c0e82318900c23734ecf18cb4e
Sebastien Derilus <sebastienderilus@Sebastiens-MBP.lan>
2023-04-30 22:00:49 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,54 @@
+const API_KEY = '5wgt1dseqr1tHAPo9VjrV4Hq9CKD1geO';
+const searchForm = document.getElementById('search-form');
+const searchHistory = document.getElementById('search-history');
+const gifGrid = document.getElementById('gif-grid');
+ 
+function fetchGifs(searchTerm) {
+  const url = `https://api.giphy.com/v1/gifs/search?api_key=${API_KEY}&q=${searchTerm}&limit=12`;
+  fetch(url)
+    .then(response => response.json())
+    .then(data => displayGifs(data.data));
+}
+ 
+function displayGifs(gifs) {
+  gifGrid.innerHTML = '';
+  gifs.forEach(gif => {
+    const gifElement = document.createElement('div');
+    gifElement.classList.add('col-md-4', 'mb-4');
+    gifElement.innerHTML = `
+      <img src="${gif.images.fixed_height.url}" class="img-fluid" alt="${gif.title}">
+    `;
+    gifGrid.appendChild(gifElement);
+  });
+}
+ 
+function addSearchButton(searchTerm) {
+  const button = document.createElement('button');
+  button.classList.add('btn', 'btn-secondary', 'me-2', 'mb-2');
+  button.textContent = searchTerm;
+  searchHistory.appendChild(button);
+}
+ 
+function loadSearchHistory() {
+  const history = JSON.parse(localStorage.getItem('searchHistory')) || [];
+  history.forEach(term => addSearchButton(term));
+}
+ 
+searchForm.addEventListener('submit', event => {
+  event.preventDefault();
+  const searchTerm = event.target.elements['search-input'].value;
+  fetchGifs(searchTerm);
+  addSearchButton(searchTerm);
+ 
+  const history = JSON.parse(localStorage.getItem('searchHistory')) || [];
+  history.push(searchTerm);
+  localStorage.setItem('searchHistory', JSON.stringify(history));
+});
+ 
+searchHistory.addEventListener('click', event => {
+  if (event.target.tagName === 'BUTTON') {
+    fetchGifs(event.target.textContent);
+  }
+});
+ 
+loadSearchHistory();
